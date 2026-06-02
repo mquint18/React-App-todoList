6b993125afcc86f02c1a66afeca11f5df194af29
Michael <michaelquint@gmail.com>
2026-06-02 14:58:52 -0400

    new image link

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     return(
         <div className='head-div'>
            
-            <img src="src/assets/doodle.png" alt="ToDo List" className='header-image'/>
+            <img src="mquint18.github.io/assets/doodle.png" alt="ToDo List" className='header-image'/>
         </div>
     );
 }
